009bf375ac174c29ddeea0b14f252d607e32b6ef
Ajan <zkac219@live.rhul.ac.uk>
2023-11-22 17:11:52 +0000

    Implementing API which contains quotes

--- Game.jsx
+++ Game.jsx
@@ -1,7 +1,25 @@
 import "./Game.css";
 import Navbar from "./pro-typer/src/navbar";
+import { useEffect, useState } from "react";
+const TEXT_API_URL = "https://api.quotable.io/random";
 
 function Game() {
+  function randomText() {
+    return fetch(TEXT_API_URL)
+      .then((response) => response.json())
+      .then((data) => data.content);
+  }
+  async function newText() {
+    const text = await randomText();
+    setCurrentText(text);
+  }
+
+  const [currentQuote, setCurrentText] = useState("");
+
+  useEffect(() => {
+    newText();
+  }, []);
+
   return (
     <>
       <Navbar></Navbar>
@@ -10,7 +28,9 @@ function Game() {
           <div className="time" id="timer">
             Timer: 0
           </div>
-          <div className="text-container" id="quote"></div>
+          <div className="text-container" id="quote">
+            {currentQuote}
+          </div>
           <input id="text" autoFocus></input>
         </div>
       </div>
