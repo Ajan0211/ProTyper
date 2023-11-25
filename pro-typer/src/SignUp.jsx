6b48de5dc68ec6cb8d1beb67130666fe80f41bbb
Ajan <zkac219@live.rhul.ac.uk>
2023-11-25 19:57:39 +0000

    Adding html skeleton for signup page

--- pro-typer/src/SignUp.jsx
+++ pro-typer/src/SignUp.jsx
@@ -5,6 +5,46 @@ function SignUp() {
   return (
     <>
       <Navbar></Navbar>
+      <div className="container-signup">
+        <div className="heading-signup">Sign up</div>
+        <div className="input">
+          <input
+            type="firstname"
+            id="fname"
+            name="firstname"
+            placeholder="firstname..."
+          ></input>
+        </div>
+
+        <div className="input">
+          <input
+            type="lastname"
+            id="lname"
+            name="lastname"
+            placeholder="Lastname..."
+          ></input>
+        </div>
+
+        <div className="input">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter Email..."
+          ></input>
+        </div>
+        <div className="input">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password..."
+          ></input>
+        </div>
+        <div className="button-container2">
+          <div className="signup-button">Sign up</div>
+        </div>
+      </div>
     </>
   );
 }
