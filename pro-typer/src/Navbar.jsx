2dab18de56464996dac00f1e888b4b8022f174a4
Ajan <zkac219@live.rhul.ac.uk>
2023-11-25 16:10:16 +0000

    Changing layout of navbar

--- pro-typer/src/Navbar.jsx
+++ pro-typer/src/Navbar.jsx
@@ -5,18 +5,20 @@ function Navbar() {
   const navigate = useNavigate();
   return (
     <div className="navbar">
-      <img className="logo" src="src/assets/Layer 1.png"></img>
+      <div className="nav-left">
+        <div className="nav-item1">
+          <a onClick={() => navigate("/")}>Home</a>
+        </div>
+        <div className="nav-item1">
+          <a onClick={() => navigate("/Shop")}>Shop</a>
+        </div>
+        <div className="nav-item1">
+          <a onClick={() => navigate("/Settings")}>Settings</a>
+        </div>
+      </div>
 
-      <div className="nav-item">
-        <a onClick={() => navigate("/")}>Home</a>
-      </div>
-      <div className="nav-item">
-        <a onClick={() => navigate("/Settings")}>Settings</a>
-      </div>
-      <div className="nav-item">
-        <a onClick={() => navigate("/Shop")}>Shop</a>
-      </div>
-      <div className="nav-item">
+      <img className="logo" src="src/assets/Layer 1.png"></img>
+      <div className="nav-right">
         <a onClick={() => navigate("/Login")}>Login/Signup</a>
       </div>
     </div>
