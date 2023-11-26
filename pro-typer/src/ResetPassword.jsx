3350dbcc79ef59b674cda03a3ece6aae35ef4d47
Ajan <zkac219@live.rhul.ac.uk>
2023-11-26 14:30:33 +0000

    making html skeleton code for reset password page

--- pro-typer/src/ResetPassword.jsx
+++ pro-typer/src/ResetPassword.jsx
@@ -5,6 +5,25 @@ function ResetPassword() {
   return (
     <>
       <Navbar></Navbar>
+      <div className="container-resetpassword">
+        <div className="heading-resetpassword">Reset Password</div>
+        <div className="reset-instructions">
+          Enter your email to receive a password reset link. Note: The link will
+          be sent only if there is an account associated with the provided email
+          on Protyper.
+        </div>
+        <div className="input">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter Email..."
+          ></input>
+        </div>
+        <div className="continue-button">
+          <a onClick={() => navigate("/Login")}>Continue</a>
+        </div>
+      </div>
     </>
   );
 }
