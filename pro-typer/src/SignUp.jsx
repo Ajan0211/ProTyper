3dbce920be46fed760bad0d88d80d3a575383f38
Ajan <zkac219@live.rhul.ac.uk>
2023-11-26 15:56:49 +0000

    Fixing issue with centering sign up container

--- pro-typer/src/SignUp.jsx
+++ pro-typer/src/SignUp.jsx
@@ -11,54 +11,56 @@ function SignUp() {
   return (
     <>
       <Navbar></Navbar>
-      <div className="container-signup">
-        <div className="heading-signup">Sign up</div>
-        <div className="input">
-          <i className="fa-solid fa-user"></i>
-          <input
-            type="firstname"
-            id="fname"
-            name="firstname"
-            placeholder="Firstname..."
-          ></input>
-        </div>
+      <div className="signup-page">
+        <div className="container-signup">
+          <div className="heading-signup">Sign up</div>
+          <div className="input">
+            <i className="fa-solid fa-user"></i>
+            <input
+              type="firstname"
+              id="fname"
+              name="firstname"
+              placeholder="Firstname..."
+            ></input>
+          </div>
 
-        <div className="input">
-          <i className="fa-solid fa-user"></i>
-          <input
-            type="lastname"
-            id="lname"
-            name="Lastname"
-            placeholder="Lastname..."
-          ></input>
-        </div>
+          <div className="input">
+            <i className="fa-solid fa-user"></i>
+            <input
+              type="lastname"
+              id="lname"
+              name="Lastname"
+              placeholder="Lastname..."
+            ></input>
+          </div>
 
-        <div className="input">
-          <i className="fa-solid fa-envelope"></i>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter Email..."
-          ></input>
-        </div>
-        <div className="input">
-          <i className="fa-solid fa-key"></i>
-          <input
-            type={`${showPassword ? "text" : "password"}`}
-            id="password"
-            name="password"
-            placeholder="Password..."
-          ></input>
-          <i
-            className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"}`}
-            type="show-password"
-            onClick={() => setShowPassword((state) => !state)}
-          ></i>
-        </div>
-        <div className="button-container2">
-          <div className="signup-button">
-            <a onClick={() => navigate("/Login")}>Sign up</a>
+          <div className="input">
+            <i className="fa-solid fa-envelope"></i>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter Email..."
+            ></input>
+          </div>
+          <div className="input">
+            <i className="fa-solid fa-key"></i>
+            <input
+              type={`${showPassword ? "text" : "password"}`}
+              id="password"
+              name="password"
+              placeholder="Password..."
+            ></input>
+            <i
+              className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"}`}
+              type="show-password"
+              onClick={() => setShowPassword((state) => !state)}
+            ></i>
+          </div>
+          <div className="button-container2">
+            <div className="signup-button">
+              <a onClick={() => navigate("/Login")}>Sign up</a>
+            </div>
           </div>
         </div>
       </div>
