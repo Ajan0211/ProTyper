26fc29c47f84af207b2729844a3c1b2e68a2c604
Ajan <zkac219@live.rhul.ac.uk>
2023-11-26 18:32:07 +0000

    Making html skelelton for shop page

--- pro-typer/src/Shop.jsx
+++ pro-typer/src/Shop.jsx
@@ -5,6 +5,12 @@ function Shop() {
   return (
     <>
       <ShopNav></ShopNav>
+      <div className="page-container">
+        <div className="box-container">box 1</div>
+        <div className="box-container">box 2</div>
+        <div className="box-container">box 3</div>
+        <div className="box-container">box 4</div>
+      </div>
     </>
   );
 }
