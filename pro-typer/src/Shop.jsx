0970a160823947c0ecdac93fe96b625d05593de6
Ajan <zkac219@live.rhul.ac.uk>
2023-11-26 18:51:59 +0000

    Adding html skeleton for whats inside the box container

--- pro-typer/src/Shop.jsx
+++ pro-typer/src/Shop.jsx
@@ -6,7 +6,13 @@ function Shop() {
     <>
       <ShopNav></ShopNav>
       <div className="page-container">
-        <div className="box-container">box 1</div>
+        <div className="box-container">
+          item 1<div className="image-container"></div>
+          <div className="price-container">
+            $$$$
+            <div className="Add-button">Add to Bag</div>
+          </div>
+        </div>
         <div className="box-container">box 2</div>
         <div className="box-container">box 3</div>
         <div className="box-container">box 4</div>
