3874f47f6112e8b35d3af55b58e01e06319bbb5f
Ajan <zkac219@live.rhul.ac.uk>
2023-11-28 18:32:58 +0000

    Making placeholder text for shop-container in shopping bag

--- pro-typer/src/Bag.jsx
+++ pro-typer/src/Bag.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 const Bag = (props) => {
   const { price, popUp } = props;
+
   return (
     <div className={`PopUp ${[popUp ? "show" : ""]}`}>
       Shopping bag
       <div className="item-container">
-        <div className="shop-container"></div>
+        <div className="shop-container">
+          item1
+          <div className="quantity">
+            <i className="fa-solid fa-plus"></i>
+            <div className="quantity-num">2</div>
+            <i className="fa-solid fa-minus"></i>
+          </div>
+        </div>
         <div className="shop-container"></div>
         <div className="shop-container"></div>
         <div className="total-container">
