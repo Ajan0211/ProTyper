9a3685c376be39219496f9327bac7a8fcd1f2b9f
Ajan <zkac219@live.rhul.ac.uk>
2023-11-29 14:21:31 +0000

    Finishing card info section on checkout page

--- pro-typer/src/Checkout.jsx
+++ pro-typer/src/Checkout.jsx
@@ -9,10 +9,12 @@ function Checkout() {
       <div className="checkout-heading">
         Thank you for shopping at ProTyper!
       </div>
+
       <div className="checkout-container">
         <div className="pay-container">
+          Personal information
           <div className="personal-container">
-            Personal information
+            Please input your personal information below
             <div className="input-pay">
               <i className="fa-solid fa-user"></i>
               <input
@@ -68,7 +70,27 @@ function Checkout() {
               ></input>
             </div>
           </div>
-          <div className="card-info"></div>
+        </div>
+        <div className="card-info">
+          Card Information
+          <div className="card-container">
+            Enter card information below
+            <div class="card-input-container">
+              <div class="input-container">
+                <input
+                  placeholder="XXXX-XXXX-XXXX-XXXX"
+                  class="input-box-card"
+                  id="card-number"
+                />
+              </div>
+              <div class="input-container">
+                <input placeholder="XX/XX" class="input-box" id="exp-date" />
+              </div>
+              <div class="input-container">
+                <input placeholder="CVV" class="input-box" id="cvv" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
