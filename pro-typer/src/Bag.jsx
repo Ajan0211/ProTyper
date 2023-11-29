aa35aa2392a18db05455bafce3e747259539d2fb
Ajan <zkac219@live.rhul.ac.uk>
2023-11-29 13:12:58 +0000

    Adding functionality to shop bag items so that it adds it up and returns a sum

--- pro-typer/src/Bag.jsx
+++ pro-typer/src/Bag.jsx
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import BagItem from "./BagItem";
 
 const Bag = (props) => {
-  const { price, popUp } = props;
+  const { popUp } = props;
+  const [priceList, setPriceList] = useState([18, 6, 4]);
 
   return (
     <div className={`PopUp ${[popUp ? "show" : ""]}`}>
       Shopping bag
       <div className="item-container">
-        <div className="shop-container">
-          item1
-          <div className="quantity">
-            <i className="fa-solid fa-plus"></i>
-            <div className="quantity-num">2</div>
-            <i className="fa-solid fa-minus"></i>
-          </div>
-        </div>
-        <div className="shop-container"></div>
-        <div className="shop-container"></div>
-        <div className="total-container">
-          <div className="total">Total:</div> {price}
-        </div>
+        {priceList.map((_, index) => {
+          return (
+            <BagItem
+              priceIndex={index}
+              priceList={priceList}
+              setPriceList={setPriceList}
+              key={`bag-item-${index}`}
+            />
+          );
+        })}
+      </div>
+      <div className="total-cost">
+        Total:
+        {priceList.reduce((a, b) => {
+          return a + b;
+        })}
       </div>
     </div>
   );
