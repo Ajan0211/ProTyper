64e16c971e5b1968bda192c19ac4fa2260a7eb2e
Ajan <zkac219@live.rhul.ac.uk>
2024-02-11 18:25:53 +0000

    Adding background feature insettings page

--- pro-typer/src/Settings.jsx
+++ pro-typer/src/Settings.jsx
@@ -5,6 +5,27 @@ function Setting() {
   return (
     <>
       <Navbar></Navbar>
+      <div className="title">Fonts</div>
+      <div className="font-container">
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+        <div className="font1">font1</div>
+      </div>
+
+      <div className="title">Background</div>
+      <div className="background-container">
+        <div className="background1">Light Mode</div>
+        <div className="background1">Dark Mode</div>
+      </div>
     </>
   );
 }
