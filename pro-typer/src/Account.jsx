8b56368a098232dd646c1e3f91d88ce53e792cd7
Ajan <zkac219@live.rhul.ac.uk>
2024-02-19 17:06:08 +0000

    Making minor edits to account page

--- pro-typer/src/Account.jsx
+++ pro-typer/src/Account.jsx
@@ -17,66 +17,40 @@ function Account() {
       <div className="account-page">
         <div className="container-account">
           <div className="heading-account">My Account</div>
-          <div className="change-container">
-            <div className="left-container">
-              Change first name
-              <div className="input-account">
-                <i className="fa-solid fa-user"></i>
-                <input
-                  type="firstname"
-                  id="fname"
-                  name="firstname"
-                  placeholder="Firstname..."
-                ></input>
-              </div>
-              Change last name
-              <div className="input-account">
-                <i className="fa-solid fa-user"></i>
-                <input
-                  type="lastname"
-                  id="lname"
-                  name="Lastname"
-                  placeholder="Lastname..."
-                ></input>
-              </div>
-              Change Email
-              <div className="input-account">
-                <i className="fa-solid fa-envelope"></i>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Enter Email..."
-                ></input>
-              </div>
-            </div>
-            <div className="right-container">
-              Change last name
-              <div className="input">
-                <i className="fa-solid fa-user"></i>
-                <input
-                  type="lastname"
-                  id="lname"
-                  name="Lastname"
-                  placeholder="Lastname..."
-                ></input>
-              </div>
-              Change Email
-              <div className="input">
-                <i className="fa-solid fa-envelope"></i>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Enter Email..."
-                ></input>
-              </div>
-            </div>
+          Change first name
+          <div className="input-account">
+            <i className="fa-solid fa-user"></i>
+            <input
+              type="firstname"
+              id="fname"
+              name="firstname"
+              placeholder="Firstname..."
+            ></input>
+          </div>
+          Change last name
+          <div className="input-account">
+            <i className="fa-solid fa-user"></i>
+            <input
+              type="lastname"
+              id="lname"
+              name="Lastname"
+              placeholder="Lastname..."
+            ></input>
+          </div>
+          Change Email
+          <div className="input-account">
+            <i className="fa-solid fa-envelope"></i>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter Email..."
+            ></input>
           </div>
           <div className="button-container2">
             <div className="change-button">Make Changes</div>
-          </div>
-        </div>
+          </div>{" "}
+        </div>{" "}
       </div>
     </>
   );
