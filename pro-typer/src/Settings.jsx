e023a8e0445bc1a4c5c4e284ebacffac267bdf4e
Ajan <zkac219@live.rhul.ac.uk>
2024-02-20 20:28:40 +0000

    Changing the placeholders to fonts I will be using in settings

--- pro-typer/src/Settings.jsx
+++ pro-typer/src/Settings.jsx
@@ -10,18 +10,18 @@ function Setting() {
       <Navbar></Navbar>
       <div className="title">Fonts</div>
       <div className="font-container">
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
-        <div className="button2">font1</div>
+        <div className="button2">Kode Mono</div>
+        <div className="button2">Anta</div>
+        <div className="button2">Montserrat</div>
+        <div className="button2">Roboto Condensed</div>
+        <div className="button2">Playfair Display</div>
+        <div className="button2">Oswald</div>
+        <div className="button2">Bebas Neue</div>
+        <div className="button2">Kanit</div>
+        <div className="button2">Caveat</div>
+        <div className="button2">Lobster</div>
+        <div className="button2">Indie Flower</div>
+        <div className="button2">Tangerine</div>
       </div>
 
       <div className="title">Background</div>
