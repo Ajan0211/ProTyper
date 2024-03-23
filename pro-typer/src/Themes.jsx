ea786f802e2e0199363262d4deb7754c9ecf67fa
Ajan <zkac219@live.rhul.ac.uk>
2024-03-23 15:18:31 +0000

    Added functionaility for Themes page in My account

---
+++ pro-typer/src/Themes.jsx
@@ -0,0 +1,62 @@
+import { useContext } from "react";
+import Navbar from "../../Navbar.jsx";
+import AccountNav from "./AccountNav.jsx";
+import "./OwnedItems.css";
+import { UserContext } from "./userContext.jsx";
+import { ThemeContext } from "./themeContext.jsx";
+import { useNavigate } from "react-router-dom";
+
+function Skins() {
+  const { user } = useContext(UserContext);
+  const { currentBG, setCurrentBG } = useContext(ThemeContext);
+  const navigate = useNavigate();
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="ownedItems-page">
+        <div className="container-ownedItems">
+          <AccountNav></AccountNav>
+
+          <div className="item-section">Themes:</div>
+          <div className="owned-items-list">
+            {user?.items
+              ? user.items
+                  .filter((item) => item.type == "theme")
+                  .map((item, index) => {
+                    return (
+                      <div
+                        key={`owned-item-theme-${index}`}
+                        className={`owned-item ${
+                          currentBG == item.image ? "active" : ""
+                        }`}
+                        onClick={() => {
+                          if (currentBG == item.image) {
+                            setCurrentBG(null);
+                          } else {
+                            setCurrentBG(item.image);
+                          }
+                        }}
+                      >
+                        <img
+                          className="Owned-items-image theme-image"
+                          src={item.image}
+                          alt=""
+                        />
+                        {item.name}
+                      </div>
+                    );
+                  })
+              : "No items found..."}
+          </div>
+          <div className="button-container2">
+            <div className="change-button" onClick={() => navigate("/Game")}>
+              Head to Game
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Skins;
