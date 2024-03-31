b46b4936dbc960aa436495c20be6a8bef5aed9eb
Ajan <zkac219@live.rhul.ac.uk>
2024-03-31 20:38:01 +0100

    Added comments for UserContext.jsx

--- pro-typer/src/userContext.jsx
+++ pro-typer/src/userContext.jsx
@@ -1,34 +1,43 @@
+/**
+ * @author Ajanthapan Agilaruben
+ * This is the file that contains the contxt that contains the user info and provides it acroos all components in the aaplication
+ *
+ * @date 31/3/2024 - 13:29:50 PM
+ *
+ * @returns User context.
+ */
+
 import axios from "axios";
 import { createContext, useState, useEffect, useContext } from "react";
 import { BagContext } from "./BagContext";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-
+// this creates a new context for the users informations
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
   const { bag, setBag } = useContext(BagContext);
   const navigate = useNavigate();
-
+  // This function updates the users profile information
   const updateProfile = () => {
     axios.get("/api/profile").then(({ data }) => {
       setUser(data);
     });
   };
-
+  // This is teh function that handles the user logout
   const logout = () => {
     setUser(null);
     axios.get("/api/logout");
   };
-
+  // This isi the function that handles the checkout process
   const checkout = (dataToSend) => {
     axios
-      .post("/api/checkout", dataToSend)
+      .post("/api/checkout", dataToSend) // send the data to the backend
       .then(() => {
-        setBag([]);
-        toast.success("Purchase was successful");
-        navigate("/");
+        setBag([]); // This clears the bag after purchase went through
+        toast.success("Purchase was successful"); // This shows the success message
+        navigate("/"); // Navigates back to home page
       })
       .catch((error) => {
         if (error.response.status == 401) {
