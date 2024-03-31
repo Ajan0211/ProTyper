9bbe746f0ccfd6ee3d66e1d2b222c2ee4be36516
Ajan <zkac219@live.rhul.ac.uk>
2024-03-31 19:43:04 +0100

    Added comments for login.jsx, login.css

--- Login.jsx
+++ Login.jsx
@@ -8,9 +8,9 @@ import { toast } from "react-hot-toast";
 
 /**
  * @author Ajanthapan Agilaruben
- * This file is used for the Login page where the use would type in there email and password. There
+ * This file is used for the Login page where the user would type in there email and password. There
  * is a sign up button for un-registered users and a forgot your password section as well if the user has forgotten there passwords.
- * @date 12/5/2023 - 11:47:21 AM
+ * @date 30/3/2024 - 11:47:21 AM
  *
  * @returns {The navbar along with the login form }
  */
@@ -22,26 +22,27 @@ function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const { setUser } = useContext(UserContext);
-
+  // this is the function which is called when the user attempts to log in
   const handleSubmit = () => {
     axios
       .post("/api/Login", {
-        email,
+        email, // This includes the users email and password in the request body
         password,
       })
       .then((result) => {
+        // This checks if the result doesnt return a specific error messages
         if (
           result.data !== "the password is incorrect" &&
           result.data !== "No record existed"
         ) {
-          setUser(result.data);
-          navigate("/");
-          toast.success("Logged in!");
+          setUser(result.data); // After the user is logged in it saves it to the frontend
+          navigate("/"); // naviagtes to the home page when login is successful
+          toast.success("Logged in!"); // displays a success toast message
         } else {
-          toast.error("Incorrect email or password. Please try again!");
+          toast.error("Incorrect email or password. Please try again!"); // displays an error toast message if the email or password is wrong.
         }
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err)); // this catches any error during the request.
   };
 
   return (
@@ -65,6 +66,7 @@ function Login() {
 
         <div className="input">
           <i className="fa-solid fa-key"></i>
+          {/* when clicked it would show the password typed in and also change the icon as well */}
           <input
             type={`${showPassword ? "text" : "password"}`}
             id="password"
@@ -81,14 +83,15 @@ function Login() {
         </div>
         <div className="button-container">
           <div className="submit-button">
-            {/* This is commented out as it would set off error message. */
-            /* <a onClick={() => setShowError(true)}>Login</a> */}
+            {/* allows user to login if password and email is correct if not it would send an error message */}
             <a onClick={() => handleSubmit()}>Login</a>
           </div>
+          {/* leads user to the sign up page */}
           <div className="submit-button">
             <a onClick={() => navigate("/SignUp")}>Sign up</a>
           </div>
         </div>
+        {/* leads user to the reset password page */}
         <div className="forgot-password">
           <a onClick={() => navigate("/ResetPassword")}>
             Forgot your password?
