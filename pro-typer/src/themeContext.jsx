070f5dd426028b2f07ec39a289513e267b85e5ce
Ajan <zkac219@live.rhul.ac.uk>
2024-03-31 21:31:57 +0100

    Added commenst for themeContext.jsx

--- pro-typer/src/themeContext.jsx
+++ pro-typer/src/themeContext.jsx
@@ -1,3 +1,12 @@
+/**
+ * @author Ajanthapan Agilaruben
+ * This is the file that contains the context which manages the themes including support for light/dark mode as well as choosing the fonts.
+ *
+ * @date 31/3/2024 - 13:29:50 PM
+ *
+ * @returns Themes component
+ */
+
 import { createContext, useState } from "react";
 
 export const ThemeContext = createContext({});
@@ -7,7 +16,7 @@ export function ThemeContextProvider({ children }) {
   const [currentFont, setCurrentFont] = useState(null);
   const [currentSkin, setCurrentSkin] = useState(null);
   const [currentBG, setCurrentBG] = useState(null);
-
+  // This is the list of the available fonts
   const fontList = [
     { name: "Kode Mono", styleString: '"Kode Mono", monospace' },
     { name: "Montserrat", styleString: '"Montserrat", sans-serif' },
@@ -24,7 +33,7 @@ export function ThemeContextProvider({ children }) {
       styleString: '"Coming Soon", cursive',
     },
   ];
-
+  // This is the function that gets the CSS style string of the current font chosen by the user.
   const getCurrentFontString = () => {
     if (currentFont == null) {
       return null;
@@ -35,16 +44,16 @@ export function ThemeContextProvider({ children }) {
   return (
     <ThemeContext.Provider
       value={{
-        isLightMode,
-        setIsLightMode,
-        fontList,
-        currentFont,
-        setCurrentFont,
-        getCurrentFontString,
-        currentSkin,
-        setCurrentSkin,
-        currentBG,
-        setCurrentBG,
+        isLightMode, // whether light mode is on
+        setIsLightMode, //m function to set light/dark mode
+        fontList, // list of available fonts
+        currentFont, // currently selected font
+        setCurrentFont, // function to set the current font
+        getCurrentFontString, // function to get the style string of the current font
+        currentSkin, // currently selevted skin
+        setCurrentSkin, // Function to set the current skin
+        currentBG, // Currently selected background
+        setCurrentBG, // function to set current
       }}
     >
       {children}
