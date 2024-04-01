d11afa9bd0ebb8045cfba9df8c3ea09e2344d217
Ajan <zkac219@live.rhul.ac.uk>
2024-04-01 23:29:01 +0100

    Added comments to Home.jsx, Home.css

--- pro-typer/src/Home.jsx
+++ pro-typer/src/Home.jsx
@@ -8,10 +8,10 @@ import axios from "axios";
 /**
  * @author Ajanthapan Agilaruben
  * This file contains the Home page to the game so this would allow the user to press the button Start Game
- * and it would direct the user to the game.
- * @date 12/5/2023 - 12:50:30 PM
+ * and it would direct the user to the game and also the leaderboard which shows the users rankings based on their average speed.
+ * @date 1/4/2024 - 12:50:30 PM
  *
- * @returns { the navbar component with a short welcome text and a button which directs user to the game.}
+ * @returns { the navbar component with a short welcome text and a button which directs user to the game as well as a button that leads to the leaderboard}
  */
 function Home() {
   const navigate = useNavigate();
@@ -36,10 +36,12 @@ function Home() {
               against the clock – where speed meets precision at ProTyper!
             </div>
             <div className="home-welcome-buttons">
+              {/* This button leads to leaderboard section below */}
               <a href="#leaderboard-container" className="button">
                 <span>View Leadboard</span>
               </a>
               <div className="button">
+                {/* This button naviagtes to game page*/}
                 <a onClick={() => navigate("/Game")}>Start Game</a>
               </div>
             </div>
@@ -51,11 +53,14 @@ function Home() {
               {leaderboard.map((person, index) => {
                 return (
                   <div className="people">
+                    {/* This ranks the users */}
                     <div className="rank">{index + 1}</div>
                     <div className="name">
+                      {/* This shows the users name */}
                       <i className="fa-solid fa-user"></i> {person.firstname}{" "}
                       {person.lastname}
                     </div>
+                    {/* This shows the users wpm */}
                     <div className="fastest-wpm">{person.wpm} WPM</div>
                   </div>
                 );
